ce6c2fa4465dd0533a8df1e46be690b563c94476
Yanzen Project <yanwarsolah@gmail.com>
2023-08-25 19:54:52 +0700

    p0502 - menambahkan total harga belanjaan

--- todo.js
+++ todo.js
@@ -1,9 +1,16 @@
 let button = document.querySelector("button");
 let inputTodo = document.querySelector("#input-todo");
 let daftarItem = document.querySelector("#daftar-item");
+let inputHarga = document.querySelector("#input-harga");
+let totalHargaKeseluruhan = document.querySelector("#total-harga-keseluruhan");
+let totalHarga = 0;
 
 button.addEventListener("click", function () {
   let item = document.createElement("li");
-  item.innerText = inputTodo.value;
+  item.innerText = `${inputTodo.value} ${inputHarga.value}`;
   daftarItem.appendChild(item);
+
+  // Total Harga
+  totalHarga = totalHarga + parseInt(inputHarga.value);
+  totalHargaKeseluruhan.innerHTML = totalHarga;
 });
